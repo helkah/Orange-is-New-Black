a6759185fcd765e870773bbcd87ef61d80c0beaf
Helena <helena.halicka@gmail.com>
2017-07-12 00:34:28 +0200

    Search box, fonts, uvWidget

--- js/google-map-latlon-picker.js
+++ js/google-map-latlon-picker.js
@@ -1,6 +1,7 @@
 
 // When the window has finished loading google map fire off
 google.maps.event.addDomListener(window, 'load', init);
+  
     
 function init() {
     
@@ -55,26 +56,78 @@ function init() {
     
 var geocoder = new google.maps.Geocoder(); //access the Google Maps API geocoding service, more info "https://developers.google.com/maps/documentation/javascript/geocoding"
 
-/* Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }*/
-  };
+ ///////////////////CHECK THIS OUT////////////////////////////// 
+ 
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+    });
+    
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = []
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              title: place.name,
+              position: place.geometry.location,
+              draggable: true    
+            }));
+
+          
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+        
+        var latitudeValue = marker.getPosition().lat();
+        var longitudeValue = marker.getPosition().lng();
+        var latitudeElement = document.getElementById('gllpLatitude');
+        var longitudeElement = document.getElementById('gllpLongitude');
+        latitudeElement.innerText = latitudeValue;
+        longitudeElement.innerText = longitudeValue;
+        
+        map.fitBounds(bounds);
+        });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+};
 
 
 
