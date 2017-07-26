448e705b4fc4722eb44f206f96f86060397fcd7a
Helena <helena.halicka@gmail.com>
2017-07-26 16:31:27 +0200

    JQ code cleanup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 $(function(){
   
-  ////Variables////////
+  //1//////////VARIABLES/////////////////
     
   var uvIndexElement = $("#uvIndex");
   var latitudeInput = $("#gllpLatitude");
   var longitudeInput = $("#gllpLongitude");
-  console.log(latitudeInput.text(),longitudeInput.text())    
+  var clickHereBtnForMobile =  $('#showTrumpBtn');
+  var showMapBtnForMobile =  $('#showMapBtn'); 
+  var btnForTabletAndDesktop = $(".forTabletAndDesktopBtn");
+  var lastScrollTopValue = 0;    
     
-  ////FUNCTIONS/////
+  //2//////////FUNCTIONS/////////////////
   function loadLocationKey(){
       
       var latitudeValue = (Math.round((latitudeInput.text())*100))/100;
@@ -15,7 +18,6 @@ $(function(){
       var urlAdress = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search'
       var apiKey = '?apikey=kiK5mAnIBjvK9fWrJSTDASICEao6KrLT&q=';
       
-     // 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=kiK5mAnIBjvK9fWrJSTDASICEao6KrLT&q=51%2C17&language=pl&details=false&toplevel=true'
       
             $.ajax({
             	   url: urlAdress + apiKey + latitudeValue + "%2C" + longitudeValue + "&language=pl&details=false&toplevel=true"
@@ -24,191 +26,219 @@ $(function(){
                     console.log(response.Key);
     	    }).fail(function(error) {
             console.log(error);
-            })
+            });
       
-  }    
-    
-    
+  };    
     
   function loadUvIndex(locationKey) {
       
-
-            var urlAdress = 'http://dataservice.accuweather.com/currentconditions/v1/';
-            var apiKey = '?apikey=kiK5mAnIBjvK9fWrJSTDASICEao6KrLT&';
-            //http://dataservice.accuweather.com/currentconditions/v1/2694848?apikey=kiK5mAnIBjvK9fWrJSTDASICEao6KrLT&language=pl&details=true
+        var urlAdress = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        var apiKey = '?apikey=kiK5mAnIBjvK9fWrJSTDASICEao6KrLT&';
       
-            $.ajax({
-            	   url: urlAdress + locationKey + apiKey + "language=pl&details=true" 
-            }).done(function(response){
-                    showWhatDonaldSay(response[0].UVIndex);
-    	    }).fail(function(error) {
-            console.log(error);
-            })
-  }
+        $.ajax({
+            url: urlAdress + locationKey + apiKey + "language=pl&details=true" 
+        }).done(function(response){
+                showWhatDonaldSay(response[0].UVIndex);
+        }).fail(function(error) {
+        console.log(error);
+        });
+  };
     
   function showWhatDonaldSay(givenUvIndex){
-        var uVIndexValue = givenUvIndex;
-        console.log(uVIndexValue);
-        $("#uvIndex").text(uVIndexValue);
         
-        var donaldImageElement = $('.donaldSign')
+        var uVIndexValue = givenUvIndex; 
+        var urlAdress = '';
+        var speachText = '';
+        var protectionRuleClass = '';
+        
         $(".protectionRules").css('display','none');
       
+     
+      
         if (uVIndexValue<=2){
-            donaldImageElement.animate({'opacity': 0}, 1000, function () {
-                donaldImageElement.css('background-image','url(../images/TrumpSign_mad.png)');
-                donaldImageElement.find("span").text("This place sucks! UV Index is " + uVIndexValue );
-                $(".low").slideDown();
-                donaldImageElement.find("button").text("Try again!");
-            }).animate({'opacity': 1}, 1000);        
+            
+            urlAdress = 'url(../images/TrumpSign_mad.png)';
+            speachText = 'This place sucks! UV Index is ';
+            protectionRuleClass = '.lowProtectionRule';
+            
+            changeDonaldSpeach(urlAdress,speachText,uVIndexValue,protectionRuleClass);          
         };
         
-      
         if (uVIndexValue>2 && uVIndexValue<=5){
             
-            donaldImageElement.animate({'opacity': 0}, 1000, function () {
-                donaldImageElement.css('background-image','url(../images/TrumpSign_mad.png)');
-                donaldImageElement.find("span").text("This place sucks! UV Index is " + uVIndexValue );
-                $(".moderate").slideDown();
-                donaldImageElement.find("button").text("Try again!");
-            }).animate({'opacity': 1}, 1000);     
-    
+            urlAdress = 'url(../images/TrumpSign_mad.png)';
+            speachText = 'This place sucks! UV Index is ';
+            protectionRuleClass = '.moderateProtectionRule';
+            
+            changeDonaldSpeach(urlAdress,speachText,uVIndexValue,protectionRuleClass);
         };
                 
-      
         if (uVIndexValue>5 && uVIndexValue<=7){
-             
-            donaldImageElement.animate({'opacity': 0}, 1200, function () {
-                donaldImageElement.css('background-image','url(../images/TrumpSign_sad.png)');
-                donaldImageElement.find("span").text("Not even close! UV Index is " + uVIndexValue);
-                $(".high").slideDown();
-                donaldImageElement.find("button").text("Try again!");
-            }).animate({'opacity': 1}, 1200);     
-                
+            
+            urlAdress = 'url(../images/TrumpSign_sad.png)';
+            speachText = 'Not even close! UV Index is ';
+            protectionRuleClass = '.highProtectionRule';
+            
+            changeDonaldSpeach(urlAdress,speachText,uVIndexValue,protectionRuleClass);    
         };
       
         if (uVIndexValue>7 && uVIndexValue<=10){
             
-            donaldImageElement.animate({'opacity': 0}, 1000, function () {
-                donaldImageElement.css('background-image','url(../images/TrumpSign_laughing.png)');
-                donaldImageElement.find("span").text("Hell yeah! UV Index is " + uVIndexValue);
-                $(".veryHigh").slideDown();
-                donaldImageElement.find("button").text("Try again!");
-            }).animate({'opacity': 1}, 1000);     
+            urlAdress = 'url(../images/TrumpSign_laughing.png)';
+            speachText = 'Hell yeah! UV Index is ';
+            protectionRuleClass = '.veryHighProtectionRule';
+            
+            changeDonaldSpeach(urlAdress,speachText,uVIndexValue,protectionRuleClass);           
         };     
       
         if(uVIndexValue>10){
-           
-            donaldImageElement.animate({'opacity': 0}, 1000, function () {
-                donaldImageElement.css('background-image','url(../images/TrumpSign_laughing.png)');
-                donaldImageElement.find("span").text("We`re goona die! UV Index is " + uVIndexValue);
-                $(".extreme").slideDown();
-            }).animate({'opacity': 1}, 1000);     
-           
-        };
-                  
+            
+            urlAdress = 'url(../images/TrumpSign_laughing.png)';
+            speachText = 'We`re goona die! UV Index is ';
+            protectionRuleClass = '.extremeProtectionRule';
+            
+            changeDonaldSpeach(urlAdress,speachText,uVIndexValue,protectionRuleClass);    
+        };                  
+    };
+    
+    function changeDonaldSpeach(url,speach,uV,selector){
+        
+        var donaldImageElement = $('.donaldSign');
+        
+        donaldImageElement.animate({'opacity': 0}, 1000, function () {
+                donaldImageElement.css('background-image',url);
+                donaldImageElement.find("span").text(speach + uV);
+                $(selector).slideDown();
+                donaldImageElement.find("button").text("Try again!");
+        }).animate({'opacity': 1}, 1000);
     };
         
-  
-////EVENTS///////////    
     
-  $('#showTrump').on('click',function(){
-      
-     loadLocationKey();  
-     $('.flipper').toggleClass('rotation');
-                    
-  });
-    
-  $('#showMap').on('click',function(){
-                 $('.flipper').toggleClass('rotation');
-                console.log($('.flipper'))
-                 });
-  $(".forTabletAndDesktop").on('click',function(){
-      loadLocationKey();
-  })
-    
-     
-var lastScrollTopValue = 0;
-$(window).on('scroll',function(event){
-  var currentScrollTopValue = $(this).scrollTop();    
-  if (currentScrollTopValue > lastScrollTopValue){
-       if(currentScrollTopValue + $(window).height() == $(document).height()){
-           $('.movingTrump').css('background-image','url(../images/trump_iddle.png)').css('background-position-y','0')
-       }else{
-        $('.movingTrump').css('background-image','url(../images/trump_run.png)').css('background-position-y','0')
-       }
-   }else{
-        if(currentScrollTopValue != 0){
-            $('.movingTrump').css('background-image','url(../images/trump_run.png)').css('background-position-y','512px');
-        }else{   
-            $('.movingTrump').css('background-image','url(../images/trump_iddle.png)').css('background-position-y','0');
+   function spriteAnimation(currentScrollTopValue){
+         
+        if (currentScrollTopValue > lastScrollTopValue){ //lastScrollTopValue global declaration
+        
+            if(currentScrollTopValue + $(window).height() == $(document).height()){
+                $('.movingTrump').css('background-image','url(../images/trump_iddle.png)').css('background-position-y','0')
+            }else{
+                $('.movingTrump').css('background-image','url(../images/trump_run.png)').css('background-position-y','0')
+            };
+        
+        }else{
+        
+            if(currentScrollTopValue != 0){
+                $('.movingTrump').css('background-image','url(../images/trump_run.png)').css('background-position-y','512px');
+            }else{   
+                $('.movingTrump').css('background-image','url(../images/trump_iddle.png)').css('background-position-y','0');
+            };
         }
-   }
+       
+        lastScrollTopValue = currentScrollTopValue; //remember where scroll bar was
+    };
     
+    function makeHeaderSticky(currentScrollTopValue){ 
+        
+        var stickyNavTop = $('header').offset().top;// offset gets current coordinates relative to document (top,left)
+        
+        if (currentScrollTopValue  > stickyNavTop) { 
+            $('header > div').addClass('sticky');
+        } else {
+            $('header > div').removeClass('sticky'); 
+        }
+    };
     
-   lastScrollTopValue = currentScrollTopValue;
-    
-    ////////////STICKY MENU/////////////////
-    var stickyNavTop = $('header').offset().top;
-    if (currentScrollTopValue  > stickyNavTop) { 
-        $('header > div').addClass('sticky');
-    } else {
-        $('header > div').removeClass('sticky'); 
+    function hideHamburgerMenu(){
+        $(this).animate({opacity:0},300, function(){
+            $(this).css('visibility','hidden');
+        });
     }
-   
     
+    function showDropDownMenuShowCross(){
+        $(this).next().next().next().css({ opacity: 0, visibility: "visible"}).animate({opacity: 1}, 300);
+        $('.dropDownMenu').slideDown();
+    }
+    
+    function hideCross(){
+        $(this).animate({opacity:0},300, function(){
+            $(this).css('visibility','hidden');
+        });
+    }
+    
+    function hideDropDownMenuShowHamburgerMenu(){
+    
+        $(this).prev().prev().prev().css({opacity: 0, visibility: "visible"
+            }).animate({opacity: 1}, 300);
+        $('.dropDownMenu').slideUp();
+    }
+    
+    function scrollToTargetSection(){  
+        
+        var scrollPlaceid = $(this).children().attr('href');
+        var menuHeight = $('.dropDownMenu').height();
+        var headerHeight = $('header').height();
+
+    
+        if (headerHeight<menuHeight){ //for mobile and tablet
+            $('html,body').animate({
+                scrollTop: ($(scrollPlaceid).offset().top) - menuHeight - headerHeight,
+            },1000)
+        }else{ // for desktop
+            $('html,body').animate({
+                scrollTop: ($(scrollPlaceid).offset().top) - headerHeight,
+            },1000)
+    }
+};
+    
+//////////////EVENTS///////////////////// 
+    
+//Btn events    
+    
+  clickHereBtnForMobile.on('click',function(){
+
+        loadLocationKey();  
+        $('.flipper').toggleClass('rotation');                    
   });
     
+  showMapBtnForMobile.on('click',function(){
       
-
-
-
-////////Hamburger menu EVENTS///////
-
-$('.hamburger').on('click', function(){
-    console.log("Działa");
-    $(this).animate({
-        opacity:0
-        },300, function(){
-        $(this).css('visibility','hidden');
-    });
-    $(this).next().next().next().css({
-        opacity: 0.0, 
-        visibility: "visible"
-        }).animate({
-        opacity: 1
-        }, 300);
-    $('.dropDownMenu').slideDown();
-})
-
-$('.cross').on('click', function(){
-    console.log("Działa");
-    $(this).animate({
-        opacity:0
-        },300, function(){
-        $(this).css('visibility','hidden');
-    });
-    $(this).prev().prev().prev().css({
-        opacity: 0.0, 
-        visibility: "visible"
-        }).animate({
-        opacity: 1
-        }, 300);
-    $('.dropDownMenu').slideUp();
-})
-
-/////////Event for dropdown Menu/////////////
-
-$('li').on('click', function(){
-    var scrollPlaceid = $(this).children().attr('href');
+        $('.flipper').toggleClass('rotation');
+  });
     
-    var menuHeight = $('.dropDownMenu').height();
-    var headerHeight = $('header').height();
-                
-   $('html,body').animate({
-        scrollTop: ($(scrollPlaceid).offset().top) - menuHeight - headerHeight,
-    },1000)
-})
+    
+  btnForTabletAndDesktop.on('click',function(){
+         
+      loadLocationKey();
+  });
+    
+//Scroll bar event   
+  
+    $(window).on('scroll',function(event){
+    
+        var currentScrollTopValue = $(this).scrollTop();// scrollTop gets current vertical position of the scroll bar
+    
+        spriteAnimation(currentScrollTopValue);
+        makeHeaderSticky(currentScrollTopValue); 
+    });
+    
+//Hamburger menu and cross events
+
+    $('.hamburger').on('click', function(){   
+        hideHamburgerMenu();
+        showDropDownMenuShowCross();
+    });
+
+
+    $('.cross').on('click', function(){
+        hideCross();
+        hideDropDownMenuShowHamburgerMenu();
+    });
+
+//Event for dropdown Menu elements
+
+    $('li').on('click', function(){
+        scrollToTargetSection();
+    
+    });
 
 
 
